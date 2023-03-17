fbfbf6ffa157a9156e1da8219e92aac41ba924f3
Lev761 <mariahsvetlichnaya@yandex.ru>
2023-03-17 20:58:46 +0300

    githookUpdate

--- config/route.php
+++ config/route.php
@@ -10,3 +10,18 @@
  */
 
 use Triangle\Engine\Route;
+use support\telegram\Api;
+
+
+
+Route::any('/.githook', function ($request) {
+    if (json_decode($request->post('payload'), true)['ref'] == 'refs/heads/main') {
+        $tg = new Api('5550184609:AAF1SRbe1O6xWCbx9DKh5wFad5AuxzouIKE');
+        $tg->sendMessage(['chat_id' => 312211167, 'text' => 'hackathone_localzet поймал githook']);
+        $tg->sendMessage(['chat_id' => 1305880257, 'text' => 'hackathone_localzet поймал githook']);
+        MongoDB(null, 'log')->insertGetId(['githook' => $request->post()]);
+        exec('cd ' . base_path() . ' && sudo git reset --hard && sudo git pull && sudo php master restart');
+    }
+    return response();
+});
+
