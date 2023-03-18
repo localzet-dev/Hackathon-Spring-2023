c15f7b26313e17ad383dc56a25060b3ea6559dad
Ivan Zorin <creator@localzet.ru>
2023-03-18 16:25:02 +0300

    Update WSS.php

--- process/WSS.php
+++ process/WSS.php
@@ -8,12 +8,12 @@ class WSS
 {
     public function onConnect(TcpConnection $connection)
     {
+        $connection->send(["messages" => 8]);
         echo "onConnect\n";
     }
 
     public function onWebSocketConnect(TcpConnection $connection, $http_buffer)
     {
-        $connection->send(["messages" => 8]);
     }
 
     public function onMessage(TcpConnection $connection, $data)
