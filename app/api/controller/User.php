3e4ef9f6585fbfd0d57215e59fb7f7a24a37592d
Lev761 <mariahsvetlichnaya@yandex.ru>
2023-03-18 03:48:47 +0300

    Update User.php

--- app/api/controller/User.php
+++ app/api/controller/User.php
@@ -2,6 +2,18 @@
 
 namespace app\api\controller;
 
+use app\Model\User as ModelUser;
+
 class User{
+    function get($request){
+
+        $current = ModelUser::find(request()->user);
+         
+
+    }
+
+    function update($request){
+
+    }
     
 }
